4a9873be4619a730976dc82dc078ddc19fd21a3e
unknown <abdenour.didi@student.vinci.be>
2022-10-07 10:38:27 +0200

    seance 4 intro

---
+++ seance4/SchoolApp/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SchoolApp.Models;
+using SchoolApp.Repository;
+
+Console.WriteLine("Hello, World!");
+
+SchoolContext schoolContext = new SchoolContext();
+BaseRepositorySQL<Section> baseRepositorySQL = new BaseRepositorySQL<Section>(schoolContext);
+
+Section sectInfo = new Section { Name = "sectInfo" };
+Section sectDiet = new Section { Name = "sectDiet" };
+
+baseRepositorySQL.Save(sectInfo, (s => s.Name == "sectInfo"));
+baseRepositorySQL.Save(sectDiet, (s => s.Name == "sectDiet"));
+
+
+
+
+
+
+

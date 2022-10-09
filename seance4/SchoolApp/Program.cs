213223b954beaf63e47faa2b9022ba386209420a
unknown <abdenour.didi@student.vinci.be>
2022-10-09 17:25:51 +0200

    seance 4 modif

--- seance4/SchoolApp/Program.cs
+++ seance4/SchoolApp/Program.cs
@@ -1,17 +1,59 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using School.Repository;
 using SchoolApp.Models;
 using SchoolApp.Repository;
+using SchoolApp.UnitsOfWork;
 
-Console.WriteLine("Hello, World!");
-
+//B exo 2ab 
 SchoolContext schoolContext = new SchoolContext();
-BaseRepositorySQL<Section> baseRepositorySQL = new BaseRepositorySQL<Section>(schoolContext);
+
+IUnitOfWorkSchool unitOfWorkSchool = new UnitOfWorkSchoolMemory();
+IRepository<Section> repoSect = unitOfWorkSchool.SectionsRepository;
+IStudentRepository repoStud = unitOfWorkSchool.StudentsRepository;
+
+//BaseRepositorySQL<Section> baseRepositorySQLSection = new BaseRepositorySQL<Section>(schoolContext);
+//StudentRepositorySQLServer baseRepositorySQLStudent = new StudentRepositorySQLServer(schoolContext);
 
 Section sectInfo = new Section { Name = "sectInfo" };
 Section sectDiet = new Section { Name = "sectDiet" };
 
-baseRepositorySQL.Save(sectInfo, (s => s.Name == "sectInfo"));
-baseRepositorySQL.Save(sectDiet, (s => s.Name == "sectDiet"));
+repoSect.Save(sectInfo, (s => s.Name == "sectInfo"));
+repoSect.Save(sectDiet, (s => s.Name == "sectDiet"));
+
+
+//question
+IList<Section> listSection = baseRepositorySQLSection.GetAll();
+
+
+
+if (listSection.Count > 0)
+{
+    Console.WriteLine("ajout fais");
+}
+
+//B exo 2cd
+
+Student studinfo1 = new Student { Name = "studinfo1",  Firstname = "studinfo1", YearResult = 100, Section=sectInfo};
+Student studdiet = new Student { Name = "studdiet", Firstname = "studdiet", YearResult = 120, Section=sectDiet};
+Student studinfo2 = new Student { Name = "studinfo2", Firstname = "studinfo2", YearResult = 110, Section = sectInfo };
+
+repoStud.Save(studinfo1, (s => s.Name == "studinfo1"));
+repoStud.Save(studdiet, (s => s.Name == "studdiet"));
+repoStud.Save(studinfo2, (s => s.Name == "studinfo2"));
+
+IList<Student> students = repoStud.GetStudentBySectionOrderByYearResult();
+
+foreach(Student s in students)
+{
+    Console.WriteLine("Section {0}, Etudiant {1}, Year resultat {2}", s.Section.Name, s.Name, s.YearResult);
+}
+
+
+
+
+
+
+
 
 
 
--- seance4/SchoolApp/UnitsOfWork/UnitOfWorkSchoolsSQLServer.cs
+++ seance4/SchoolApp/UnitsOfWork/UnitOfWorkSchoolsSQLServer.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using School.Repository;
+using SchoolApp.Models;
+using SchoolApp.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,31 @@ using System.Threading.Tasks;
 
 namespace SchoolApp.UnitsOfWork
 {
-    internal class UnitOfWorkSchoolsSQLServer 
+    internal class UnitOfWorkSchoolsSQLServer : IUnitOfWorkSchool
     {
+        private readonly SchoolContext _context;
+
+        private StudentRepositorySQLServer _studentRepository;
+
+        private BaseRepositorySQL<Section> _sectionsRepository;
+
+
+        public UnitOfWorkSchoolsSQLServer(SchoolContext context)
+        {
+            this._context = context;
+            this._studentRepository = new StudentRepositorySQLServer(context);
+            this._sectionsRepository = new BaseRepositorySQL<Section>(context);
+
+        }
+        public IStudentRepository StudentsRepository
+        {
+            get { return this._studentRepository; }
+        }
+
+        public IRepository<Section> SectionsRepository
+        {
+            get { return this._sectionsRepository; }
+        }
     }
 }
+
